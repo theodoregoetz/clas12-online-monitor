0311d3687099bbff8d58ad483978d8272c6bc9b7
Johann Goetz <theodoregoetz@gmail.com>
2015-08-05 11:40:42 -0400

    minor edit

--- clas12monitor/dc/occupancy.py
+++ clas12monitor/dc/occupancy.py
@@ -5,7 +5,7 @@ from clas12monitor.data import EvioReader
 def dc_wire_occupancy(evio_source):
     dcocc = np.zeros((6,6,6,112))
     for evt in EvioReader(evio_source):
-        dchits = evt.bank('HitBasedTrkg::HBHits')
+        dchits = evt['HitBasedTrkg::HBHits']
         if dchits is not None:
             hit_data = np.vstack([
                 dchits.sector    ,
