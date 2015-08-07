8887a88049672cda5ea27e0927ee53a494114a7b
Brook Byrd <brook.byrd.13@cnu.edu>
2015-08-06 23:52:20 -0400

    update sectors

--- clas12monitor/dc/db_tab.py
+++ clas12monitor/dc/db_tab.py
@@ -16,6 +16,8 @@ class DBTab(QtGui.QTabWidget):
 
     def init_buttons(self):
 
+        self.currentChanged.connect(lambda sec: self.parent.wiremaps.setCurrentIndex(sec+1))
+
         sector_fmt     = 'sc{sector}'
         superlayer_fmt = 'sc{sector}_sl{superlayer}'
         direction_fmt  = 'sc{sector}_{direction}' #f or b
