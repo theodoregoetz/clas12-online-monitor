fffc5f708d6646538b24ffa5244d07cfa75f6adf
SArah <sarah.clark.14@cnu.edu>
2015-08-19 13:27:16 -0400

    ftof occupany

---
+++ doc/ftof/ftofoccupany.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from clas12monitor.data import EvioReader
+
+def ftof_wire_occupancy(evio_source):
+    ftofocc = np.zeros((6,60))
+    for evt in EvioReader(evio_source):
+        ftofhits = evt['FTOFRec::rawhits']
+        if dchits is not None:
+            hit_data = np.vstack([
+                ftofhits.sector    ,
+                ftofhits.panel_id,]).T - 1
+            for panel_id in hit_data:
+                ftofocc[tuple(panel_id)] += 1
+    return ftofocc
+    
+if __name__ == "__main__" :
+    ftof_wire_occupancy()
